291ab2e9be9039b92a85fe0f405df6b8856ea8c1
dhruvcdata <dagrawat@cdatainsights.com>
2025-03-27 00:24:50 +0530

    new

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'MedicBuilds',
+  description: 'Beautiful Websites for Healthcare Professionals',
+  generator: 'CdataInsights',
 }
 
 export default function RootLayout({
